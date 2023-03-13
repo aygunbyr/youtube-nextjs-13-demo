f5db4945ed3d945c1e3572191ac9fca8e0be46dd
Aygün Bayır <turkoaygun@gmail.com>
2023-03-14 00:32:27 +0300

    implemented react suspense boundaries

--- app/(users)/todos/TodosList.tsx
+++ app/(users)/todos/TodosList.tsx
@@ -2,6 +2,10 @@ import Link from "next/link";
 import { Todo } from "../../../typings";
 
 const fetchTodos = async () => {
+  // timeout for random number of seconds between 1 and 5
+  const timeout = Math.floor(Math.random() * 5 + 1) * 1000;
+  await new Promise((resolve) => setTimeout(resolve, timeout));
+
   const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
   const todos: Todo[] = await res.json();
   return todos;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
+import { Suspense } from "react";
+import TodosList from "./(users)/todos/TodosList";
+
 function Home() {
-  return <div>I am the homepage</div>;
+  return (
+    <div>
+      <Suspense fallback={<p className="text-red-500">Loading the Todos...</p>}>
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+
+      <Suspense
+        fallback={
+          <p className="text-blue-500">Loading the Shopping Trolley...</p>
+        }
+      >
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+    </div>
+  );
 }
 
 export default Home;
