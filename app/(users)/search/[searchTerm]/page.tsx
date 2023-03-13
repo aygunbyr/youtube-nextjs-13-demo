219b74adaf8de9e8d55d18acdd04bfe86ddaee06
Aygün Bayır <turkoaygun@gmail.com>
2023-03-14 00:09:12 +0300

    used next.js route groups feature

--- app/(users)/search/[searchTerm]/page.tsx
+++ app/(users)/search/[searchTerm]/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type PageProps = {
   params: {
     searchTerm: string;
--- app/(users)/todos/page.tsx
+++ app/(users)/todos/page.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import TodosList from "./TodosList";
-
 function Todos() {
   return (
     <div>
